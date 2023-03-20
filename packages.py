e44a3dceba4e98feebf725bd61a9934c94698bf1
VenkataSaiAnu13 <113762729+VenkataSaiAnu13@users.noreply.github.com>
2023-03-20 21:00:18 +0530

    Add files via upload

---
+++ packages.py
@@ -0,0 +1,28 @@
+"""class student:
+    name=""
+    roll_number=""
+    branch=""
+    marks=""
+    attendence=0 #the varibles put in the class is said to be class variables
+    is_using_transport=false
+    view_attendence()
+    view_marks()
+    view_name()
+    update_name(name_name) #the methods that we put in the class is said to be classmethod"""
+
+
+class student:
+    name=""
+    roll_number=""
+    branch=""
+    marks=""
+    attendence=0
+    is_using_transport=false
+    def view_attendence(self):
+        pass
+    def view_marks(self):
+        pass
+    def view_name(self):
+        pass
+    def update_name(self,new_name):
+        pass
