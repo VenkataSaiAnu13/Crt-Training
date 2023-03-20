281fdd00b121c445ed7aa0959f564d93e5051bf7
VenkataSaiAnu13 <113762729+VenkataSaiAnu13@users.noreply.github.com>
2023-03-20 21:00:53 +0530

    Add files via upload

---
+++ POLYMORPHISM.py
@@ -0,0 +1,16 @@
+class animal:
+    def eat(self):
+        pass
+    def sound(self):
+        print('sound')
+    def dog(animal):
+        print(' dog barks')
+        pass
+    def lion(animal):
+        print('lion roars')
+        pass
+    def cat(animal):
+        print('meow meow')
+
+obj=animal()
+obj.cat()
